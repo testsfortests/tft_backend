f98878e9d2e2e2841b937e6f0e84328035962b6e
testsfortests <158587957+testsfortests@users.noreply.github.com>
2024-02-29 13:15:27 +0530

    Update env for data sheet

--- config/env.js
+++ config/env.js
@@ -4,6 +4,9 @@ export const BASE_URL = 'https://tft-backend.vercel.app/'
 
 export const SPREADSHEET_KEY="14PY1yNdKIImeoKPikhH9RNMcLrD8WK7tev8CjEre51I"
 export const SHEET_NAME="SHEET2!A1:Z"
+
+export const DATA_SPREADSHEET_KEY="178Jr7nTOk24Iac-f3-OdXz9vWgmLFp1D419B0w00Meg"
+export const DATA_SHEET_NAME="DATA!A1:Z"
 export const SHEET_SCOPE_URL = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 export const SHEET_URL = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_KEY}/values/${SHEET_NAME}`;
 export const API_KEY = "AIzaSyAGAU7w5whpGwWbI-Cw6m1_6cqc0AkYuDM"
