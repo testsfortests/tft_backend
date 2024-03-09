786d847943b63618c112ffe94d8be120d9db63e5
testsfortests <158587957+testsfortests@users.noreply.github.com>
2024-03-09 07:06:11 +0000

    removed bot init

--- utils/telegram.js
+++ utils/telegram.js
@@ -1,30 +1,30 @@
-import TelegramBot from 'node-telegram-bot-api';
-import {SEND_MSG_URL} from "./constants.js"
-import axios from 'axios';
+// import TelegramBot from 'node-telegram-bot-api';
+// import {SEND_MSG_URL} from "./constants.js"
+// import axios from 'axios';
 
-export function initializeTelegramBot() {
-    const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+// export function initializeTelegramBot() {
+//     const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-    bot.on('message',async (msg) => {
-        const chatId = msg.chat.id; // admin addition #
-        const messageText = msg.text;
+//     bot.on('message',async (msg) => {
+//         const chatId = msg.chat.id; // admin addition #
+//         const messageText = msg.text;
 
-        // console.log(`Received message from ${chatId}: ${messageText}`);
-        if (messageText === "msg") {
-        const messageParams = {
-            chat_id: process.env.TESTING_CHAT_ID, 
-            text: "This is a Testing Message from BOT"
-        };
-        const response = await axios.post(SEND_MSG_URL, messageParams);
-        }
-        bot.sendMessage(chatId, messageText)
-            .then(() => {
-                console.log('Message sent successfully',messageText);
-            })
-            .catch(error => {
-                console.error('Error sending message:', error.message);
-            });
-    });
+//         // console.log(`Received message from ${chatId}: ${messageText}`);
+//         if (messageText === "msg") {
+//         const messageParams = {
+//             chat_id: process.env.TESTING_CHAT_ID, 
+//             text: "This is a Testing Message from BOT"
+//         };
+//         const response = await axios.post(SEND_MSG_URL, messageParams);
+//         }
+//         bot.sendMessage(chatId, messageText)
+//             .then(() => {
+//                 console.log('Message sent successfully',messageText);
+//             })
+//             .catch(error => {
+//                 console.error('Error sending message:', error.message);
+//             });
+//     });
 
-    return bot;
-}
+//     return bot;
+// }
