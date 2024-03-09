5463a65392b18df1c79610c4c98053710b828cdb
testsfortests <158587957+testsfortests@users.noreply.github.com>
2024-03-09 07:57:28 +0000

    refactoring

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import axios from 'axios';
 const router = express.Router();
-
 import dotenv from "dotenv" 
 dotenv.config()
 
 import {SEND_MSG_URL} from "../utils/constants.js"
 
 router.get('/cron', async (req, res) => {
+    console.log("TEST CRON CALLED...")
     try {
         const message = req.body && req.body.message ? req.body.message :  "This is a testing message !"  ;        
         const chatId = req.body && req.body.chatId ? req.body.chatId :  process.env.TESTING_CHAT_ID  ;        
@@ -16,6 +16,7 @@ router.get('/cron', async (req, res) => {
             chat_id: chatId, 
             text: message
         };
+        console.log("test",SEND_MSG_URL,chatId)
         const response = await axios.post(SEND_MSG_URL, messageParams);
         res.status(200).json({ success: true,message:'Message sent successfully', data: response.data });
     } catch (error) {
